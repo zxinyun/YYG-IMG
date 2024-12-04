707d30c120804ee4e84d2a0246a63c7cffe2b79b
Han <1655466387@qq.com>
2024-12-04 13:06:02 +0800

    优化结构🆒更新UI🎈

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,14 @@
-import './assets/less/base.less'
-
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
+
+// 基础样式
+import '@/assets/less/main.less'
+// tailwind-base
+import '@/assets/tailwind-base.css'
 
 const app = createApp(App)
-// 屏蔽黄色警告信息
-app.config.warnHandler = () => null
+
+app.use(router)
 
 app.mount('#app')
--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,23 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// 按需引入
-import { vitePluginForArco } from '@arco-plugins/vite-vue'
+import autoprefixer from 'autoprefixer'
+import tailwind from 'tailwindcss'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vitePluginForArco()],
-  resolve: { alias: { '@': fileURLToPath(new URL('./src', import.meta.url)) } },
-  server: { host: '0.0.0.0' },
-  base: "./"
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()],
+    },
+  },
+  plugins: [
+    vue(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+  server: { host: '0.0.0.0' }
 })
