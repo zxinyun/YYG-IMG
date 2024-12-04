37b01ce7d13b488702317363413b377642ccc5ce
Han <1655466387@qq.com>
2024-12-04 13:23:27 +0800

    优化局部样式🍍

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -2,7 +2,7 @@
   <footer class="w-full max-w-[1666px] mt-36 pb-4">
     <main class="m-auto pt-4 flex flex-col-reverse md:flex-row justify-between gap-2 md:gap-16 w-full max-w-[1666px] border-t">
       <section class="[&>p]:text-xs [&>p]:w-full [&>p]:py-[6px] [&>p]:text-center md:[&>p]:text-left [&>p>a]:text-slate-400">
-        <p>Han IPFS Files 是 <a href="https://www.vvhan.com" target="_blank" title="韩小韩博客">韩小韩博客</a> 支持并维护的文件上传项目，致力于为用户提供稳定的永久存储服务。</p>
+        <p>骤雨重山图床 是 <a href="https://www.vvhan.com" target="_blank" title="韩小韩博客">韩小韩博客</a> 支持并维护的文件上传项目，致力于为用户提供稳定的永久存储服务。</p>
         <p class="text-red-600">注意:上传违反中国大陆、香港及美国法律的图片将会直接删除，并封禁设备IP。</p>
         <p class="flex gap-2">
           <a href="https://pages.cloudflare.com" target="_blank">
@@ -34,7 +34,7 @@
             <img class="w-8" src="@/assets/images/footer/blog.svg" />
           </a>
         </div>
-        <span class="w-full font-bold tracking-[0.3rem] text-lg text-center md:text-right">Han IPFS Files</span>
+        <span class="w-full font-bold text-lg text-center">骤雨重山图床</span>
       </section>
     </main>
   </footer>
